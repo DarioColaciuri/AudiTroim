aee9a00561fac745c782dd59f847e75dc49177aa
dariocolaciuri <darioguitarras@gmail.com>
2023-12-19 11:29:19 -0300

    agregada funcion en logo

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -15,8 +15,8 @@ return (
     <nav>
         <div id="menu-categorias" className="menu-wrapper">
         <ul className="menu">
-            <li><Link to="/"><img className="logo" src="./src/assets/logo.png" alt="logo" /></Link></li>
-            <li><Link to="/"><h1 className="logo-text">AUDITROIM</h1></Link></li>
+            <li><Link to="/"><img className="logo" src="./src/assets/logo.png" alt="logo" onClick={() => hacerClick('todos')} /></Link></li>
+            <li><Link to="/"><h1 className="logo-text" onClick={() => hacerClick('todos')}>AUDITROIM</h1></Link></li>
             <li><Link to="/"><button id="todos" className={`boton-menu boton-categoria ${activarBoton === 'todos' ? 'active' : ''}`} onClick={() => hacerClick('todos')}><img className="hand" src="./src/assets/hand.png" alt="hand icon" /> Todos</button></Link></li>
             <li><Link to="/productos/guitarras"><button id="guitarras" className={`boton-menu boton-categoria ${activarBoton === 'guitarras' ? 'active' : ''}`} onClick={() => hacerClick('guitarras')}><img className="guitar" src="./src/assets/guitar.png" alt="guitar icon" />Guitarras</button></Link></li>
             <li><Link to="/productos/bajos"><button id="bajos" className={`boton-menu boton-categoria ${activarBoton === 'bajos' ? 'active' : ''}`} onClick={() => hacerClick('bajos')}><img className="bass" src="./src/assets/bass.png" alt="bass icon" />Bajos</button></Link></li>
